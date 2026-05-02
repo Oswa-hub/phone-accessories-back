b14e0e6313f87ee7aa57822b07f796f6bf953ba0
yahyabenther <yahyabentaher45@gmail.com>
2026-05-02 11:11:48 +0100

    Initial Spring Boot backend setup

---
+++ src/test/java/phone_accessories/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package phone_accessories.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
